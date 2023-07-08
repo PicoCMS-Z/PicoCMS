39ba1011033c313d1e10d587f63d1cb157ef8cb2
aoki_taquan <104122712+aoki-taquan@users.noreply.github.com>
2023-07-08 13:59:58 +0000

    書き方を変えた

--- src/router/api.rs
+++ src/router/api.rs
@@ -2,7 +2,7 @@ use axum::routing::get;
 use axum::Router;
 
 pub fn api_routes() -> Router {
-    Router::new().route("/",get(hello_world))
+    Router::new().route("/", get(hello_world))
 }
 
 async fn hello_world() -> &'static str {
