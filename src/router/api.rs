ce04d0924cbde8a7821f0e573be45e9205c9496f
aoki_taquan <104122712+aoki-taquan@users.noreply.github.com>
2023-07-08 06:31:44 +0000

    add API routes for user management

---
+++ src/router/api.rs
@@ -0,0 +1,10 @@
+use axum::routing::get;
+use axum::Router;
+
+pub fn api_routes() -> Router {
+    Router::new().route("/",get(hello_world))
+}
+
+async fn hello_world() -> &'static str {
+    "Hello, World!"
+}
