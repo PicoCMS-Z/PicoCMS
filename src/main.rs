1cb5e9011309c31f3e86947fe84a31f5197f382b
aoki_taquan <104122712+aoki-taquan@users.noreply.github.com>
2023-07-14 05:01:56 +0000

    add sample State code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use axum::Router;
+use axum::{extract::State, Router};
+// use mongodb::bson::Document;
 
+mod database;
 mod router;
+
+mod model;
+// use crate::database::Db;
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().nest("/api", router::api::api_routes());
+    let app = Router::new()
+        .nest("/api", router::api::api_routes())
+        .with_state(database::Db::new().await);
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
