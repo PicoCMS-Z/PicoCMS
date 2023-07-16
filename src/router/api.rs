259eaedcd88c487955767217de4b5daa2ac73a05
aoki_taquan <104122712+aoki-taquan@users.noreply.github.com>
2023-07-16 10:35:00 +0000

    🔥 sample handler
    crate::api::userにhandlerは全て置き換え、sampleはもういらない

--- src/router/api.rs
+++ src/router/api.rs
@@ -1,30 +1,10 @@
-use axum::extract::State;
-use axum::response::Json;
 use axum::routing::get;
 use axum::Router;
-use mongodb::bson;
-use mongodb::bson::Document;
 
 use crate::database::Db;
 
-use super::super::model::user::User;
+
 
 pub fn api_routes() -> Router<Db> {
-    Router::new()
-        .route("/", get(hello_world))
-        .route("/user", get(get_user))
-}
 
-// sample handler
-async fn hello_world() -> &'static str {
-    "Hello, World!"
-}
-
-
-//sample detabase handler
-async fn get_user(State(db): State<Db>) -> Json<User> {
-    let user_collection = db.0.collection::<Document>("user");
-    let user = user_collection.find_one(None, None).await.unwrap().unwrap();
-    let user: User = bson::from_bson(bson::Bson::Document(user)).unwrap();
-    Json(user)
 }
