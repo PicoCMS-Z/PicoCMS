f0d4db7602c132116e2553267c381e42e2a5397e
aoki_taquan <104122712+aoki-taquan@users.noreply.github.com>
2023-07-16 11:17:14 +0000

    🎨 change api endpoint /api/{} -> /api/v0-alpha/user/{}
    user以外の操作を考えて
    変更するのが確実なのでv0-alpha

--- src/router/api.rs
+++ src/router/api.rs
@@ -7,10 +7,13 @@ use crate::database::Db;
 use crate::api;
 
 pub fn api_routes() -> Router<Db> {
-    Router::new() 
+    let user_ruter=    Router::new() 
         .route("/create", post(api::user::create))
         .route("/read", get(api::user::read_all))
         .route("/read/:id", get(api::user::read_one))
         .route("/update/:id", post(api::user::update))
-        .route("/delete/:id", post(api::user::delete))
+        .route("/delete/:id", post(api::user::delete));
+
+    Router::new()
+        .nest("/v0-alpha/user", user_ruter)
 }
