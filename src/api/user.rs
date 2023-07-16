093d8ef123c6053f35a41b69518adc4c66ff8271
aoki_taquan <104122712+aoki-taquan@users.noreply.github.com>
2023-07-16 10:57:30 +0000

    ✨ add handler read_all
    crudのrの全て取得するパターン

--- src/api/user.rs
+++ src/api/user.rs
@@ -1,6 +1,7 @@
 use axum::extract::State;
 use axum::{extract, response};
 // cursorのイテレータを使うため
+use futures::StreamExt;
 use mongodb::bson::{self, Document};
 
 use crate::database::Db;
@@ -30,5 +31,21 @@ pub async fn create(
         .unwrap();
 
     response::Json(user)
+}
+
+pub async fn read_all(State(db): State<Db>) -> response::Json<Users> {
+    let user_collection = db.0.collection("user");
+
+    let cursor = user_collection.find(None, None).await.unwrap();
+
+    let users = cursor
+        .map(|result| {
+            let doc = result.unwrap();
+            bson::from_bson(bson::Bson::Document(doc)).unwrap()
+        })
+        .collect()
+        .await;
+
+    response::Json(Users(users))
 }
 
--- src/router/api.rs
+++ src/router/api.rs
@@ -3,9 +3,10 @@ use axum::Router;
 
 use crate::database::Db;
 
-
+use crate::api;
 
 pub fn api_routes() -> Router<Db> {
     Router::new() 
         .route("/create", post(api::user::create))
+        .route("/read", get(api::user::read_all))
 }
