5b001d219656cfd3d24aa0ff1586c0e6fc1b5b99
aoki_taquan <104122712+aoki-taquan@users.noreply.github.com>
2023-07-16 10:59:36 +0000

    ✨ add handler read_one
    crudのrの一つだけのパターン
    /read/{}の{}にidでアクセス

--- src/router/api.rs
+++ src/router/api.rs
@@ -9,4 +9,5 @@ pub fn api_routes() -> Router<Db> {
     Router::new() 
         .route("/create", post(api::user::create))
         .route("/read", get(api::user::read_all))
+        .route("/read/:id", get(api::user::read_one))
 }
