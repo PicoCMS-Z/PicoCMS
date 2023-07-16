1b427c2aa37ae4d56a9552e6462024796549326a
aoki_taquan <104122712+aoki-taquan@users.noreply.github.com>
2023-07-16 10:49:28 +0000

    ✨ add create hendler
    crudのc

--- src/api/user.rs
+++ src/api/user.rs
@@ -1,1 +1,34 @@
+use axum::extract::State;
+use axum::{extract, response};
+// cursorのイテレータを使うため
+use mongodb::bson::{self, Document};
+
+use crate::database::Db;
+use crate::model::user::*;
+
+// TODO:error handling
+
+pub async fn create(
+    State(db): State<Db>,
+    extract::Json(nwe_user): extract::Json<NewUser>,
+) -> response::Json<User> {
+    let user_collection = db.0.collection::<Document>("user");
+
+    let user = User {
+        id: bson::oid::ObjectId::new(),
+        name: nwe_user.name,
+        email: nwe_user.email,
+        password: nwe_user.password,
+    };
+
+    let serialized_user = bson::to_bson(&user).unwrap();
+    let serialized_user = serialized_user.as_document().unwrap();
+
+    user_collection
+        .insert_one(serialized_user.clone(), None)
+        .await
+        .unwrap();
+
+    response::Json(user)
+}
 
--- src/router/api.rs
+++ src/router/api.rs
@@ -6,5 +6,6 @@ use crate::database::Db;
 
 
 pub fn api_routes() -> Router<Db> {
-
+    Router::new() 
+        .route("/create", post(api::user::create))
 }
