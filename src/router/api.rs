5841b0948260ca70519c091ab00b361bb21df6eb
aoki_taquan <104122712+aoki-taquan@users.noreply.github.com>
2023-07-16 05:01:02 +0000

    🔥 sample handler

--- src/router/api.rs
+++ src/router/api.rs
@@ -7,24 +7,8 @@ use mongodb::bson::Document;
 
 use crate::database::Db;
 
-use super::super::model::user::User;
+
 
 pub fn api_routes() -> Router<Db> {
-    Router::new()
-        .route("/", get(hello_world))
-        .route("/user", get(get_user))
-}
-
-// sample handler
-async fn hello_world() -> &'static str {
-    "Hello, World!"
-}
-
-
-//sample detabase handler
-async fn get_user(State(db): State<Db>) -> Json<User> {
-    let user_collection = db.0.collection::<Document>("user");
-    let user = user_collection.find_one(None, None).await.unwrap().unwrap();
-    let user: User = bson::from_bson(bson::Bson::Document(user)).unwrap();
-    Json(user)
+    Router::new() 
 }
