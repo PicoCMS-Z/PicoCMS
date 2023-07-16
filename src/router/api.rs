2b99e8d45333e4a77296341864bfc0f51e5d1b00
aoki_taquan <104122712+aoki-taquan@users.noreply.github.com>
2023-07-16 13:26:12 +0000

    🎨 エントリーポイントの統合
    urlではなくリクエストメソッドで対応する
    https://github.com/aoki-taquan/pico-cms/pull/4#issuecomment-1637054614

--- src/router/api.rs
+++ src/router/api.rs
@@ -1,4 +1,4 @@
-use axum::routing::{get,post};
+use axum::routing::{delete, get, post, put};
 
 use axum::Router;
 
@@ -7,13 +7,15 @@ use crate::database::Db;
 use crate::api;
 
 pub fn api_routes() -> Router<Db> {
-    let user_ruter=    Router::new() 
-        .route("/create", post(api::user::create))
-        .route("/read", get(api::user::read_all))
-        .route("/read/:id", get(api::user::read_one))
-        .route("/update/:id", post(api::user::update))
-        .route("/delete/:id", post(api::user::delete));
+    Router::new().nest("/v0-alpha/users", user_router())
+}
 
-    Router::new()
-        .nest("/v0-alpha/user", user_ruter)
+fn user_router() -> Router<Db> {
+    Router::new().route("/", get(api::user::read_all)).route(
+        "/:id",
+        get(api::user::read_one)
+            .post(api::user::create)
+            .put(api::user::update)
+            .delete(api::user::delete),
+    )
 }
